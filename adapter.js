4dad77cba6d5e995d78eff7a1d18b284690aea7a
Ran Ding <randing89@gmail.com>
2015-02-16 11:18:26 +1100

    Copy chai config form karma.config.client.chai

--- adapter.js
+++ adapter.js
@@ -1,6 +1,16 @@
 var should;
 
 (function(window) {
+  // Copy config to chai
+  var chaiConfig = window.__karma__.config.chai;
+  if (chaiConfig) {
+    for (var key in chaiConfig) {
+      if (chaiConfig.hasOwnProperty(key)) {
+        window.chai.config[key] = chaiConfig[key];
+      }
+    }
+  }
+
   window.should = window.chai.should();
   window.expect = window.chai.expect;
   window.assert = window.chai.assert;
